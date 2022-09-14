03e20609e1a36d001446c75e7abfd71804e665d2
nursultansadyrbekov <nurs.sadyrbekovv@gmail.com>
2022-09-15 01:29:50 +0600

    HW #2

--- HW #2/main.swift
+++ HW #2/main.swift
@@ -1,11 +1,66 @@
-//
-//  main.swift
-//  HW #2
-//
-//  Created by Нурсултан Садырбеков on 14/9/22.
-//
+// HW #2
 
-import Foundation
+// Задача №1
+// а) Нахождение квадрата и куба числа
+//func squareAndQube(num: Int) {
+//    print("Квадрат = " , num * num)
+//    print("Куб = " , num * num * num)
+//}
+//
+//print("Введите цифру")
+//let num = readLine()!
+//
+//
+//squareAndQube(num: Int (num) ?? 0)
 
-print("Hello, World!")
+// б) Нахождение периметра куба
 
+//func perimeterOfQube (num:Int) {
+//    print("Периметр куба = " , num * 12)
+//}
+//print("Длина ребра")
+//let num = readLine()!
+//
+//perimeterOfQube(num:  Int (num) ?? 0)
+
+// в) Нахождение площади прямоугольника
+
+//func squareOfRectangle (length: Int , width: Int) {
+//    print("Площадь прямоугольника = " , length * width , "кв. м")
+//}
+//print("Введите длину в м")
+//let length = readLine()!
+//print("Введите ширину в м")
+//let width = readLine()!
+//
+//squareOfRectangle(length: Int (length) ?? 0, width: Int (width) ?? 0)
+
+// г) Нахождение площади круга
+
+//func squareOfCircle (radius: Double){
+//    print("Площадь круга = " , radius * radius * pi)
+//}
+//
+//print("Введите длину Радиуса")
+//let radius = readLine()!
+//let pi = 3.141592
+//squareOfCircle(radius: Double (radius) ?? 0)
+
+
+// Задача №2
+
+//func calculator(a: Double , b: Double){
+//    print("a * b = " , a * b)
+//    print("a / b = " , a / b)
+//    print("a + b = " , a + b)
+//    print("a - b = " , a - b)
+//}
+//
+//print("Введите цифры для калькуляции")
+//
+//print("Цифра a= ")
+//let a = readLine()!
+//print("Цифра b= ")
+//let b = readLine()!
+//
+//calculator(a: Double (a) ?? 0, b: Double (b) ?? 0)
